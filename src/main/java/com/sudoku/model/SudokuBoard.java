0c0327df4eb11c197be7e8e897180ca486da1743
Thuong23130327 <23130327@st.hcmuaf.edu.vn>
2026-05-03 15:52:18 +0700

    cau truc thu muc chung

---
+++ src/main/java/com/sudoku/model/SudokuBoard.java
@@ -0,0 +1,45 @@
+package com.sudoku.model;
+
+/**
+ * [MODEL] Cấu trúc dữ liệu dùng chung cho toàn dự án.
+ * Thống nhất đánh số theo dải UR-1.x, 2.x, 3.x
+ */
+public class SudokuBoard {
+    // Ma trận 9x9 chứa trạng thái hiện tại người chơi đang nhập [UR-2.2]
+    private int[][] currentMatrix;
+    
+    // Ma trận 9x9 chứa đề bài gốc (để không cho phép xóa/sửa) [UR-2.4]
+    private boolean[][] isFixed;
+    
+    // Ma trận 9x9 chứa đáp án hoàn chỉnh (dùng cho Hint và Check Win) [UR-3.3, 4.1]
+    private int[][] solutionMatrix;
+
+    public SudokuBoard() {
+        this.currentMatrix = new int[9][9];
+        this.isFixed = new boolean[9][9];
+        this.solutionMatrix = new int[9][9];
+    }
+
+    // --- GETTERS & SETTERS ---
+    
+    public int getValueAt(int row, int col) {
+        return currentMatrix[row][col];
+    }
+
+    public void setValueAt(int row, int col, int value) {
+        // Chỉ cho phép sửa nếu ô đó không phải là số mặc định của đề bài
+        if (!isFixed[row][col]) {
+            currentMatrix[row][col] = value;
+        }
+    }
+
+    public boolean isFixedCell(int row, int col) {
+        return isFixed[row][col];
+    }
+
+    public void setFixed(int row, int col, boolean fixed) {
+        this.isFixed[row][col] = fixed;
+    }
+    
+    // Các bạn phụ trách UC-01, UC-03 sẽ viết thêm logic vào đây
+}
---
+++ src/main/java/com/sudoku/view/GridPanel.java
@@ -0,0 +1,5 @@
+package main.java.com.sudoku.view;
+
+public class GridPanel {
+    
+}
